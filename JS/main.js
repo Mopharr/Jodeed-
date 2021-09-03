7d1000b0573980b1929d8b0f32c62c3fc8c356f9
quadri salawudeen <olamiquadri1@gmail.com>
2021-09-03 15:04:02 +0100

    commit

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,17 @@ hamburger.addEventListener("click", () => {
     link.classList.toggle("fade");
   });
 });
+
+ var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
